a62ed777d9ef863b9bd8635a21c89d7ae782121e
Dennis Erdmann <dennis@erdmann-freunde.de>
2021-07-16 12:53:19 +0200

    add manager plugin

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace ContaoNutshell\Core\ContaoManager;
+namespace ContaoNutshell\Framework\ContaoManager;
 
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\CoreBundle\ContaoCoreBundle;
-use ContaoNutshell\Framework\ContaoManagerCore;
+use ContaoNutshell\Framework\ContaoNutshellFramework;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(ContaoManagerCore::class)
+            BundleConfig::create(ContaoNutshellFramework::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
