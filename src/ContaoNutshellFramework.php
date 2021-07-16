60ee455c2d0c2b513685a85aea4c0c1871c42c45
Dennis Erdmann <dennis@erdmann-freunde.de>
2021-07-16 12:40:09 +0200

    make bundle

---
+++ src/ContaoNutshellFramework.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Nutshell Framework.
+ */
+
+namespace ContaoNutshell\Framework;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoNutshellFramework extends Bundle
+{
+}
