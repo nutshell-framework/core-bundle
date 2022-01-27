e201df382d84d22e73e672e876931cfe782e3d69
Dennis Erdmann <dennis@erdmann-freunde.de>
2022-01-27 11:41:08 +0100

    add div container to headline options

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Nutshell Framework.
+ */
+
+
+// add div to headline options
+$GLOBALS['TL_DCA']['tl_module']['fields']['headline']['options'][] = 'div';
