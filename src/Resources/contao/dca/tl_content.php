c0191c6990386559d687e959063a7a95288cf2e2
Dennis Erdmann <dennis@erdmann-freunde.de>
2026-05-23 13:05:08 +0200

    Dachzeile (supline) für Headline- und Text-Inhaltselemente
    Fügt ein optionales supline-Feld (inputUnit mit h3/h4/h5) an die
    Paletten headline und text an und rendert es über einen neuen
    'supline'-Block oberhalb der eigentlichen Überschrift in der
    _headline-Komponente.

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -5,6 +5,24 @@ declare(strict_types=1);
  * This file is part of Contao Nutshell Framework.
  */
 
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 // add <div> to headline options
 $GLOBALS['TL_DCA']['tl_content']['fields']['headline']['options'][] = 'div';
+
+// Dachzeile (supline) above the headline
+PaletteManipulator::create()
+    ->addField('supline', 'type')
+    ->applyToPalette('headline', 'tl_content')
+    ->applyToPalette('text', 'tl_content')
+;
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['supline'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['supline'],
+    'exclude'   => true,
+    'search'    => true,
+    'inputType' => 'inputUnit',
+    'options'   => ['h3', 'h4', 'h5'],
+    'eval'      => ['maxlength' => 200, 'basicEntities' => true, 'tl_class' => 'w50 clr'],
+    'sql'       => "varchar(255) NOT NULL default 'a:2:{s:5:\"value\";s:0:\"\";s:4:\"unit\";s:2:\"h5\";}'",
+];
