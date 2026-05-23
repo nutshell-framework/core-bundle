22397815a264508f0250d8652550be857b320baa
Dennis Erdmann <dennis@erdmann-freunde.de>
2026-05-23 13:30:00 +0200

    Dachzeile als kombinierte Headline mit Spans rendern
    Vereinfacht das supline-Feld zu einem einfachen text-Feld (kein
    inputUnit mehr) und rendert Dachzeile, Separator und Hauptüberschrift
    als drei spans innerhalb des bestehenden Headline-Tags. Der Separator
    steht im Markup, damit Screenreader den optischen Trenner mitlesen.

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -21,8 +21,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['supline'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['supline'],
     'exclude'   => true,
     'search'    => true,
-    'inputType' => 'inputUnit',
-    'options'   => ['h3', 'h4', 'h5'],
-    'eval'      => ['maxlength' => 200, 'basicEntities' => true, 'tl_class' => 'w50 clr'],
-    'sql'       => "varchar(255) NOT NULL default 'a:2:{s:5:\"value\";s:0:\"\";s:4:\"unit\";s:2:\"h5\";}'",
+    'inputType' => 'text',
+    'eval'      => ['maxlength' => 200, 'basicEntities' => true, 'tl_class' => 'w50'],
+    'sql'       => "varchar(255) NOT NULL default ''",
 ];
